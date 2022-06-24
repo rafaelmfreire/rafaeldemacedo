9d8af357526d6557ed35838bf436e1fc6f44fcd7
Rafael de Macêdo <r4faelmf@gmail.com>
2022-06-24 20:15:43 -0300

    tweak the projects tag design

--- pages/index.vue
+++ pages/index.vue
@@ -96,21 +96,21 @@
         <div class="grid grid-cols-1 space-y-14">
           <article>
             <div class="space-y-3 mb-4">
-              <h3 class="font-bold">Tudo novo no front <span class="text-sm font-medium text-auxiliary-success-dark ml-4 opacity-60 hover:opacity-100 bg-auxiliary-success-light px-3 py-1 rounded-full before:mr-2 before:content-[''] before:h-2 before:w-2 before:left-1 before:inline-block before:rounded-full before:bg-auxiliary-success">ativo</span></h3>
+              <h3 class="font-bold">Tudo novo no front <span class="text-xs uppercase font-semibold text-auxiliary-success-dark ml-4 opacity-60 hover:opacity-100 bg-auxiliary-success-light px-3 py-1 rounded-full before:mr-2 before:content-[''] before:h-2 before:w-2 before:left-1 before:inline-block before:rounded-full before:bg-auxiliary-success">ativo</span></h3>
               <p class="font-medium text-sm text-gray-500">Como criar um site do zero com TailwindCSS configurando um Design System</p>
             </div>
             <nuxt-link to="/" class="text-xsp font-semibold text-blue-600 uppercase">Leia mais</nuxt-link>
           </article>
           <article>
             <div class="space-y-3 mb-4">
-              <h3 class="font-bold">Morf <span class="text-sm font-medium text-auxiliary-alert-dark ml-4 opacity-60 hover:opacity-100 bg-auxiliary-alert-light px-3 py-1 rounded-full before:mr-2 before:content-[''] before:h-2 before:w-2 before:left-1 before:inline-block before:rounded-full before:bg-auxiliary-alert">pausa</span></h3>
+              <h3 class="font-bold">Morf <span class="text-xs uppercase font-semibold text-auxiliary-alert-dark ml-4 opacity-60 hover:opacity-100 bg-auxiliary-alert-light px-3 py-1 rounded-full before:mr-2 before:content-[''] before:h-2 before:w-2 before:left-1 before:inline-block before:rounded-full before:bg-auxiliary-alert">pausa</span></h3>
               <p class="font-medium text-sm text-gray-500">O jeito mais fácil de transformar sua database do Notion em um formulário</p>
             </div>
             <nuxt-link to="/" class="text-xsp font-semibold text-blue-600 uppercase">Leia mais</nuxt-link>
           </article>
           <article>
             <div class="space-y-3 mb-4">
-              <h3 class="font-bold">Interfy <span class="text-sm font-medium text-auxiliary-error-dark ml-4 opacity-60 hover:opacity-100 bg-auxiliary-error-light px-3 py-1 rounded-full before:mr-2 before:content-[''] before:h-2 before:w-2 before:left-1 before:inline-block before:rounded-full before:bg-auxiliary-error">cancelado</span></h3>
+              <h3 class="font-bold">Interfy <span class="text-xs uppercase font-semibold text-auxiliary-error-dark ml-4 opacity-60 hover:opacity-100 bg-auxiliary-error-light px-3 py-1 rounded-full before:mr-2 before:content-[''] before:h-2 before:w-2 before:left-1 before:inline-block before:rounded-full before:bg-auxiliary-error">cancelado</span></h3>
               <p class="font-medium text-sm text-gray-500">Extensão do Google Chrome para alterar todas as fontes de um site para a fonte Inter</p>
             </div>
             <nuxt-link to="/" class="text-xsp font-semibold text-blue-600 uppercase">Leia mais</nuxt-link>
