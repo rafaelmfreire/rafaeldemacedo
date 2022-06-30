c854095f1ee07744836926dc2ce7b4479cd5a88f
Rafael de Macêdo <r4faelmf@gmail.com>
2022-06-30 00:04:53 -0300

    create index page for projects list

---
+++ pages/projetos/index.vue
@@ -0,0 +1,27 @@
+<template>
+	<main class="px-6">
+		<div class="container mx-auto">
+			<section class="py-14 max-w-3xl w-full mx-auto">
+				<div class="space-y-4 mb-20 flex flex-col items-center">
+					<h2 class="text-lg text-center font-medium">Projetos</h2>
+          <p class="text-center">Alguns dos projetos pessoais que criei <span class="block">Uns ativos, outros aposentados, em pausa ou encerrados</span></p>
+				</div>
+				<!-- <div class="grid grid-cols-1 space-y-14"> -->
+        <div class="grid grid-cols-1 gap-6 mini:grid mini:grid-cols-2 mini:space-y-0">
+          <Project v-for="project in projects" :project="project" :key="project.slug" />
+				</div>
+			</section>
+		</div>
+	</main>
+</template>
+<script>
+export default {
+  layout: "blog",
+  async asyncData({ $content, params }) {
+    const projects = await $content("projetos").sortBy("createdAt", "desc").fetch();
+    return {
+      projects,
+    };
+  },
+};
+</script>
