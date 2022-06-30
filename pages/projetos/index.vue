c6a74ff76f9d7d83e1875a5c306e3379aac9fb10
Rafael de Macêdo <r4faelmf@gmail.com>
2022-06-30 08:35:09 -0300

    fix background color for consistency

--- pages/projetos/index.vue
+++ pages/projetos/index.vue
@@ -1,14 +1,13 @@
 <template>
-	<main class="px-6">
+	<main class="bg-gray-200 px-6">
 		<div class="container mx-auto">
 			<section class="py-14 max-w-3xl w-full mx-auto">
 				<div class="space-y-4 mb-20 flex flex-col items-center">
 					<h2 class="text-lg text-center font-medium">Projetos</h2>
-          <p class="text-center">Alguns dos projetos pessoais que criei <span class="block">Uns ativos, outros aposentados, em pausa ou encerrados</span></p>
+					<p class="text-center">Alguns dos projetos pessoais que criei <span class="block">Uns ativos, outros aposentados, em pausa ou encerrados</span></p>
 				</div>
-				<!-- <div class="grid grid-cols-1 space-y-14"> -->
-        <div class="grid grid-cols-1 gap-6 mini:grid mini:grid-cols-2 mini:space-y-0">
-          <Project v-for="project in projects" :project="project" :key="project.slug" />
+				<div class="grid grid-cols-1 gap-6 mini:grid mini:grid-cols-2 mini:space-y-0">
+					<Project v-for="project in projects" :project="project" :key="project.slug" />
 				</div>
 			</section>
 		</div>
@@ -16,7 +15,7 @@
 </template>
 <script>
 export default {
-  layout: "blog",
+  layout: "default",
   async asyncData({ $content, params }) {
     const projects = await $content("projetos").sortBy("createdAt", "desc").fetch();
     return {
