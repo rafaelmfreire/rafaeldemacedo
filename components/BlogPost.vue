70e6675070e8c41959c0901407a5a71b1f7233d4
Rafael de Macêdo <r4faelmf@gmail.com>
2022-06-30 21:25:25 -0300

    fix responsive layout

--- components/BlogPost.vue
+++ components/BlogPost.vue
@@ -5,7 +5,7 @@
       <nuxt-link :to="`/blog/${post.slug}`" :class="{ 'group-hover:underline' : !hover }" class=" mb-3 block">
         <h3 class="text-md font-semibold">{{ post.title }}</h3>
       </nuxt-link>
-      <div :class="{ 'desktop:mt-5' : hover }" class="relative desktop:absolute desktop:bg-gray-100/20 desktop:rounded-md desktop:border desktop:border-gray-400/40 desktop:p-3 desktop:top-0 desktop:left-auto desktop:right-full space-x-4 desktop:space-x-0 mb-4 desktop:mb-0 desktop:space-y-2 mr-6 flex desktop:flex-col items-center desktop:items-end " >
+      <div :class="{ 'desktop:mt-5' : hover }" class="relative desktop:absolute desktop:bg-gray-100/20 desktop:rounded-md desktop:border desktop:border-gray-400/40 desktop:p-2 desktop:top-0 desktop:left-auto desktop:right-full space-x-4 desktop:space-x-0 mb-4 desktop:mb-0 desktop:space-y-2 mr-4 flex desktop:flex-col items-center desktop:items-end " >
         <time class="text-gray-600">{{ new Date(post.createdAt).toLocaleDateString() }}</time>
         <span class=" relative text-xsp font-medium uppercase py-1 px-2 bg-gray-100/20 border rounded-md border-gray-400/40 desktop:p-0 desktop:bg-none desktop:border-none inline-block desktop:block " >{{ post.tag }}</span >
       </div>
@@ -24,7 +24,7 @@ export default {
 	computed: {
 		classObject: function () {
 			return {
-				'py-5 px-8 rounded-md cursor-pointer hover:bg-gray-200/30 transition-colors duration-200 ease-in-out': this.hover,
+				'py-0 px-0 desktop:py-5 desktop:px-8 rounded-md cursor-pointer hover:bg-gray-200/30 transition-colors duration-200 ease-in-out': this.hover,
 			}
 		}
 	}
---
+++ pages/contato.vue
@@ -0,0 +1,36 @@
+<template>
+  <main class="bg-gray-200 px-6 flex flex-1">
+    <div class="container mx-auto">
+      <section class="py-14 max-w-lg w-full mx-auto">
+        <h1 class="text-lg font-bold mb-8">Contato</h1>
+
+        <form action="" class="space-y-4">
+          <div>
+            <label for="name" class="block text-sm font-medium text-gray-700" >Nome</label >
+            <input type="text" name="name" id="name" autocomplete="name" class=" mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-400/60 rounded-md " />
+          </div>
+          <div>
+            <label for="email" class="block text-sm font-medium text-gray-700" >Email</label >
+            <input type="email" name="email" id="email" autocomplete="given-name" class=" mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-400/60 rounded-md " />
+          </div>
+          <div>
+            <label for="message" class="block text-sm font-medium text-gray-700">Mensagem</label>
+            <div class="mt-1">
+              <textarea id="message" name="message" rows="3" class="shadow-sm focus:ring-blue-500 focus:border-blue-500 mt-1 block w-full sm:text-sm border border-gray-400/60 rounded-md"></textarea>
+            </div>
+          </div>
+          <div class="pt-4">
+            <button type="submit" class="inline-flex justify-center py-3 px-5 border border-transparent shadow-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">Enviar mensagem</button>
+          </div>
+        </form>
+      </section>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  layout: "default",
+  name: "ContactPage",
+};
+</script>
