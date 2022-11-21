6229243635c48196380daf389447507e0c514300
Rafael de Macêdo <r4faelmf@gmail.com>
2022-11-20 22:34:52 -0300

    add robots meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,6 +16,7 @@ export default {
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: "description", name: "description", content: "" },
       { name: "format-detection", content: "telephone=no" },
+      { hid: 'robots', name: 'robots', content: 'index, follow' }
     ],
     link: [{ rel: "icon", type: "image/svg", href: "/favicon.svg" }],
   },
